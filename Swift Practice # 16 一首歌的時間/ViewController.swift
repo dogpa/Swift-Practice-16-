f5f9b017ba01c57aed61f71d47d818cfac2a9037
DogpaChen <dogpa0130@gmail.com>
2021-06-27 11:50:20 +0800

    Initial Commit

---
+++ Swift Practice # 16 一首歌的時間/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 16 一首歌的時間
+//
+//  Created by CHEN PEIHAO on 2021/6/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
